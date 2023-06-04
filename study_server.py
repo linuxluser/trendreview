5ce320bfee12b84c3531c6d4f0fd0ec91bbaa6b5
linuxluser <dave@xps.feltonfarm.net>
2023-06-04 15:26:04 -0700

    Adds radio buttons to study_review.html that update study.yaml dynamically

--- study_server.py
+++ study_server.py
@@ -5,14 +5,13 @@
 
 import os
 
+import filelock
 import flask
 import yaml
 
 
 STUDIES_DIR = 'studies'
-APP = flask.Flask(__name__,
-                  static_url_path='',
-                  template_folder='templates')
+APP = flask.Flask(__name__, static_url_path='', template_folder='templates')
 
 
 @APP.route('/')
@@ -35,10 +34,32 @@ def studies_list():
 
 @APP.route('/studies/<study_date>/<image_name>.png')
 def chart(study_date, image_name):
+    """Serves PNG files directly."""
     img_path = os.path.join(STUDIES_DIR, study_date, f'{image_name}.png')
     if not os.path.exists(img_path):
         return flask.abort(404)
     return flask.send_file(img_path, mimetype='image/png')
+
+
+@APP.route('/studies/<study_date>/<symbol>/trend', methods=['POST'])
+def update_trend(study_date, symbol):
+    """Updates the Trend value in the study.yaml file."""
+    trend = flask.request.form.get('trend')
+    if trend is None:
+        return
+    if trend not in ('uptrend', 'downtrend', 'sideways'):
+        return
+    study_yaml_file = os.path.join(STUDIES_DIR, study_date, 'study.yaml')
+    lock = filelock.FileLock(study_yaml_file + '.lock')
+    with lock:
+        with open(study_yaml_file, 'r') as f:
+            symbols = yaml.load(f, Loader=yaml.CSafeLoader)
+        if symbol in symbols:
+            symbols[symbol]['Trend'] = trend
+        with open(study_yaml_file, 'w') as f:
+            yaml.dump(symbols, f, Dumper=yaml.CSafeDumper, default_flow_style=False, explicit_start=True)
+    print(f'File {study_yaml_file} updated with Trend={trend} for {symbol}')
+    return flask.request.form
 
 
 @APP.route('/studies/<study_date>')
