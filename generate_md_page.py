c6bd7e73d8d1f38ed100b1c1e8f4f70cfa1bd5a9
linuxluser <dave@xps.feltonfarm.net>
2023-06-25 23:35:00 -0700

    Adds past data to tables in generated markdown study pages

--- generate_md_page.py
+++ generate_md_page.py
@@ -4,25 +4,46 @@
 
 
 import argparse
+import datetime
 import os
 import pathlib
+import time
 
 import jinja2
 import yaml
 
 
+STUDIES_BASE_DIR = None
 STUDY_DATA_FILE = None
 STUDY_MARKDOWN_FILE = None
 
 
 def set_global_values():
     """Read config.yaml and load in global values of this script from that."""
+    global STUDIES_BASE_DIR
     global STUDY_DATA_FILE
     global STUDY_MARKDOWN_FILE
     with open('config.yaml') as f:
         config = yaml.load(f, Loader=yaml.CSafeLoader)
+    STUDIES_BASE_DIR = config['Studies Base Directory']
     STUDY_DATA_FILE = config['Study Data File']
     STUDY_MARKDOWN_FILE = config['Study Markdown File']
+
+
+def load_study_data(study_date):
+    if study_date is None:
+        return {}
+    study_directory = os.path.join(STUDIES_BASE_DIR, study_date)
+    if not os.path.exists(study_directory):
+        flask.abort(404)
+    yaml_file = os.path.join(study_directory, STUDY_DATA_FILE)
+    with open(yaml_file, 'r') as f:
+        symbols = yaml.load(f, Loader=yaml.CSafeLoader)
+    # Transform data from by-symbol to by-basket
+    baskets = {d['Basket']:{} for d in symbols.values()}
+    for symbol, data in symbols.items():
+        baskets[data['Basket']][symbol] = data
+    return baskets
 
 
 def get_date_string_from_study_directory(study_directory):
@@ -48,22 +69,47 @@ def get_date_string_from_study_directory(study_directory):
     return '{}-{:02}-{:02}'.format(year, month, day)
 
 
+def find_past_study_dates(this_study_date, back=3):
+    """Find all of the studies in the past in date order from this study date."""
+    study_dates = [d for d in os.listdir(STUDIES_BASE_DIR) if os.path.isdir(os.path.join(STUDIES_BASE_DIR, d))]
+    study_dates.sort()
+    idx = study_dates.index(this_study_date)
+    try:
+        return study_dates[idx - back:idx]
+    except IndexError:
+        return None
+
+
+def add_custom_filters(j2_environment):
+    """Add custom filters to a jinja2 environment.
+    See https://jinja.palletsprojects.com/en/3.0.x/api/#custom-filters
+    """
+    def datestr2monthday(dstr):
+        d = datetime.date(*time.strptime(dstr, '%Y-%m-%d')[:3])
+        return d.strftime('%b %d')
+    j2_environment.filters['datestr2monthday'] = datestr2monthday
+
+
 def main(args):
     set_global_values()
-    yaml_file = os.path.join(args.study_directory, STUDY_DATA_FILE)
-    with open(yaml_file, 'r') as f:
-        symbols = yaml.load(f, Loader=yaml.CSafeLoader)
+    study_date = get_date_string_from_study_directory(args.study_directory)
+    baskets = load_study_data(study_date)
+    past_study_dates = find_past_study_dates(study_date)
+    past_baskets = {sd:load_study_data(sd) for sd in past_study_dates}
+    previous_basket = past_baskets[past_study_dates[-1]]
 
-    # Transform data from by-symbol to by-basket
-    baskets = {d['Basket']:{} for d in symbols.values()}
-    for symbol, data in symbols.items():
-        baskets[data['Basket']][symbol] = data
-    for_date = get_date_string_from_study_directory(args.study_directory)
+    # Load and render template
     md_template = open('templates/study.md.jinja2').read()
-    template = jinja2.Environment(loader=jinja2.BaseLoader).from_string(md_template)
+    j2env = jinja2.Environment(loader=jinja2.BaseLoader)
+    add_custom_filters(j2env)
+    template = j2env.from_string(md_template)
     md_file = os.path.join(args.study_directory, STUDY_MARKDOWN_FILE)
     with open(md_file, 'w') as f:
-        f.write(template.render(baskets=baskets, for_date=for_date))
+        f.write(template.render(
+            baskets=baskets,
+            past_baskets=past_baskets,
+            previous_basket=previous_basket,
+            study_date=study_date))
     print(f'File written: {md_file}')
 
 
